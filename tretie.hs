18182669dc02b66ac0386da946a1ad946abf31a7
Ferko Mrkvicka <v.brecka@gmail.com>
2012-03-17 02:26:02 +0100

    v3

--- tretie.hs
+++ tretie.hs
@@ -3,16 +3,19 @@
 data Date = Date Int Int Int deriving (Show)
 data Sex = Male | Female deriving (Eq)
 
+
 initialize :: Date -> Sex -> Date
 initialize (Date y m d) s	| (s == Female) = (Date y (m+50) d)
 			 	| otherwise = (Date y m d)
 
-findFirst :: Date -> Int -> Int
-findFirst (Date y m d) postfix 	| ((y+m+d+postfix) `mod` 11 == 0) = postfix
-				| otherwise = findFirst (Date y m d) (succ postfix)
+findFirstPostfix :: Date -> Int -> Int
+findFirstPostfix (Date y m d) postfix 	| ((y+m+d+postfix) `mod` 11 == 0) = postfix
+					| otherwise = findFirstPostfix (Date y m d) (succ postfix)
 
-findAll :: Int -> [Int]
-findAll postfix = [postfix + (11 * i) | i <- [0..], postfix + (11 * i) < 10000]
+findAllPostfixes :: Int -> [Int]
+findAllPostfixes p	| (p < 10000) = p : findAllPostfixes (p+11)
+			| otherwise = []
+
 
 format :: Date -> Int -> String
 format (Date y m d) postfix = show y ++ (format' m 2) ++ (format' d 2) ++ "/" ++ (format' postfix 4) ++ "\n"
@@ -22,8 +25,12 @@ format' number expectedDigits = zeros ++ show number
 	where	zeros = [ '0' | _ <- [1..n]]
 		n = expectedDigits - length (show number)
 
-generate :: Date -> Sex -> IO()
-generate (Date y m d) s = putStrLn (concat (take 20 (map (format (initialize (Date y m d) s)) (findAll (findFirst (initialize (Date y m d) s) 0)))))
---generate (Date y m d) s = putStrLn (concat (map (format (initialize (Date y m d) s)) (findAll (findFirst (Date y m d) 0))))
 
--- interval toho cyklu a skraslit kod
+generate :: Date -> Sex -> IO()
+generate (Date y m d) s = putStrLn formattedString
+	where	--formattedString = concat (take 20 formattedList)
+		formattedString = concat formattedList
+		formattedList = map (format initializedDate) listOfPostfixes
+		listOfPostfixes = findAllPostfixes firstPostfix
+		firstPostfix = findFirstPostfix initializedDate 0
+		initializedDate = initialize (Date y m d) s
